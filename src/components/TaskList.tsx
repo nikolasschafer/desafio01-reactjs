c42d49879a39174bb01e77974a1e27ba0b0dd7ee
Nikolas <nikolas.schaffer@effecti.com.br>
2021-04-06 20:40:08 -0300

    primeiro desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,38 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const current_tasks = tasks.map(task => {
+      if(task.id === id){
+        task.isComplete = true;
+      }
+
+      return task;
+    });
+
+    setTasks(current_tasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const current_tasks = tasks.filter(task => task.id !== id);
+
+    setTasks(current_tasks);
   }
 
   return (
